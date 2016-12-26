5854e4dbf59f8ab43dfe20f4a07b4f9a67aa31d9
JACK <ho19951230@yahoo.com.tw>
2016-12-26 12:11:16 +0800

    Initial Commit

---
+++ hw3 - MRT Stations/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hw3 - MRT Stations
+//
+//  Created by JACK on 2016/12/26.
+//  Copyright © 2016年 JACK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
